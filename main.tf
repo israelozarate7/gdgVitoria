68759f76f940a28451ea884779da9bc00c9aecb6
Israel Ortiz de Zarate <israelortizdezarate@MacBook-Pro-de-Israel.local>
2023-02-19 17:00:14 +0100

    Add Function App

--- main.tf
+++ main.tf
@@ -21,3 +21,12 @@ resource "azurerm_app_service_plan" "example" {
     size = "F1"
   }
 }
+
+resource "azurerm_function_app" "example" {
+  name                       = "gdgvitoriaazurefunction"
+  location                   = var.resource_group_location
+  resource_group_name        = azurerm_resource_group.rg.name
+  app_service_plan_id        = azurerm_app_service_plan.example.id
+  storage_account_name       = azurerm_storage_account.example.name
+  storage_account_access_key = azurerm_storage_account.example.primary_access_key
+}
