6fc424a3f128678601467c5ff905f940944796df
Israel Ortiz de Zarate <israelortizdezarate@MacBook-Pro-de-Israel.local>
2023-02-19 16:48:39 +0100

    Add storage account and app service plan

--- main.tf
+++ main.tf
@@ -2,3 +2,22 @@ resource "azurerm_resource_group" "rg" {
   location = var.resource_group_location
   name     = var.resource_group_name
 }
+
+resource "azurerm_storage_account" "example" {
+  name                     = "gdgvitoriastorageaccount"
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = var.resource_group_location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
+resource "azurerm_app_service_plan" "example" {
+  name                = "gdgvitoriaappserviceplan"
+  location            = var.resource_group_location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  sku {
+    tier = "Free"
+    size = "F1"
+  }
+}
